4db0437ee5e78691b28e1f4697587e3d27030b13
Jose Gurtubay <josegurtubaymoreno@gmail.com>
2021-03-19 07:44:35 -0600

    Cambio rama 2

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -2,4 +2,11 @@ import cv2
 import numpy as np
 
 #Cargar la imagen a color
-IRGB = cv2.imread
+IRGB = cv2.imread("002.jpg")
+print(IRGB)
+print(IRGB.shape)
+
+print("Lineas agregadas en la rama 2")
+IGS = cv2.cvtColor(IRGB,cv2.COLOR_BGR2GRAY)
+print(IGS)
+print(IGS.shape)
