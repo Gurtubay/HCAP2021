2de196622b4c3a60f0a85ff419477a42a7f1387b
Jose Gurtubay <josegurtubaymoreno@gmail.com>
2021-03-19 07:48:31 -0600

    Modificacion rama main

--- cargaImagenes.py
+++ cargaImagenes.py
@@ -2,4 +2,8 @@ import cv2
 import numpy as np
 
 #Cargar la imagen a color
-IRGB = cv2.imread
+IRGB = cv2.imread('002.jpg')
+print(IRGB)
+print(IRGB.shape)
+print('modificacion en la rama main')
+print(len(IRGB))
